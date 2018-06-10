b1dd8be243dd91640ce16413a9624f5d22c90e35
Raghavendra Manjegowda <raghavendrahm0410@gmail.com>
2018-06-10 11:29:38 -0700

    [feat] Docker login password from stdin
    Now instead of entering username and password while running push
    script, just run script and enter password using stdin (read -s).
    Username deaults to my repo
    
    Signed-off-by: Raghavendra Manjegowda <raghavendrahm0410@gmail.com>

--- DockerImage/push.sh
+++ DockerImage/push.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 #Check the arguments to configure openvpn as client or server
-if [ $# -ne 2 ]
+if [ $# -ne 0 ]
   then
-    echo "argument error, usage: './push.sh username password' "
+    echo "argument error, usage: './push.sh' "
     exit 1
 fi
 
 dt=$(date '+%Y%m%d');
 
-docker login -u=$1 -p=$2
+docker login -u=raag079
 docker tag baseimage:latest raag079/docker:latest
 docker tag raag079/docker:latest raag079/docker:$dt
 
